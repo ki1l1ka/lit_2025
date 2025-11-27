fa07c9909b35a0a1df64b851d7e3cd0314b09c29
ki1l1ka <linge.ilia@lit1533.ru>
2025-11-27 22:15:30 +0300

    multiplication and addition added

--- Homeworks/Homework_3/cs/Polynom.cs
+++ Homeworks/Homework_3/cs/Polynom.cs
@@ -30,24 +30,49 @@ class Polynomial
     public override string ToString()
     {
         string polynom = "1";
-        for (int x = 1; x <= degree; x++)
+        for (int x = 1; x <= this.degree; x++)
         {
-            if (coeffs[x] == 0){continue;}
+            if (this.coeffs == {0} | this.coeffs == {}) return "";
+            if (this.coeffs[x] == 0){continue;}
             if (x == 1)
             {
-                polynom = polynom + (coeffs[x] < 0? " - " + Math.Abs(coeffs[x]) + ^ + x : " + " coeffs[x] + ^ + x);
+                polynom = polynom + (this.coeffs[x] < 0? " - " + Math.Abs(this.coeffs[x]) + '^' + x : " + " this.coeffs[x] + '^' + x);
             }
             else
-            {polynom = polynom + (coeffs[x] < 0? " - " + Math.Abs(coeffs[x]) + ^ + x : " + " coeffs[x] + ^ + x);}
+            {polynom = polynom + (this.coeffs[x] < 0? " - " + Math.Abs(this.coeffs[x]) + '^' + x : " + " this.coeffs[x] + '^' + x);}
 
         }
         return polynom;
     }
+    public static Polynomial operator +(Polynomial obj1, Polynomial obj2)
+    {
+        int max = obj1.Length > obj2.Length? obj1.Length: obj2.Length;
+        sum = new double[max];
+        for (int i = 0; i < max; i++)
+        {   
+            double elem = 0;
+            if (i < obj1.Length) {elem+= obj1[i];}
+            if (i < obj2.Length) {elem+= obj2[i];}
+            sum += elem;
+        }
+        return sum;
+    }
+    public static Polynomial operator * (Polynomial obj1, double k)
+    {
+        product = new double[obj1.Length-1];
+        for (int i = 0; i < obj1.Length; i++)
+        {
+            product[i] = obj1[i] * k;
+        }
+        return product;
+    }
+
+
 }
 class Programm{
     static void Main()
     {
-        double[] l = [2, 3, 5, 3, 5 - 34, 4, -4, 0, -1];
+        double[] l = {2, 3, 5, 3, 5 - 34, 4, -4, 0, -1};
         Console.WriteLine("hi");
         Polynomial p = new Polynomial(l);
         Console.WriteLine(p.ToString());
